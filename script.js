9f48f1d19f4043d5defce5d69bb710c44c4a06ea
kendall <kendallcoleman@Kendalls-Air.ncat.edu>
2022-11-14 15:39:28 -0500

    quick change

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
+var saveBtn = document.querySelector(".saveBtn")
+
+
+
+
+
 $(document).ready(function () {
     // listen for save button clicks
-    $('.saveBtn').on('click', function () {
-      // get nearby values
-      var value = $(this).siblings('.description').val();
-      var time = $(this).parent().attr('id');
-  
-      // save in localStorage
-      localStorage.setItem(time, value);
-  
-      // Show notification that item was saved to localStorage by adding class 'show'
-      $('.notification').addClass('show');
-  
-      // Timeout to remove 'show' class after 5 seconds
-      setTimeout(function () {
-        $('.notification').removeClass('show');
-      }, 5000);
-    });
+    save
   
     function hourUpdater() {
       // get current number of hours
